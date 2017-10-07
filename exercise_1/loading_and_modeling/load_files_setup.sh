73227816f65b50bd29ffd5e137f82071c83326b2
ram-bala <rbalasub@gmail.com>
2017-10-07 12:37:56 -0400

    moved files around

---
+++ exercise_1/loading_and_modeling/load_files_setup.sh
@@ -0,0 +1,7 @@
+#This file does 3 things
+#1. Set up the directory structure to store the data
+#2. Download the remote file
+#3. Unzip the downloaded file into the data directory
+
+#save current directory
+
