d9c934b671555ee92b32b882527c2399837eb818
ram-bala <rbalasub@gmail.com>
2017-10-07 12:37:30 -0400

    moved files around

--- exercise_1/load_data_lake.sh
+++
@@ -1,2 +0,0 @@
-wget "https://data.medicare.gov/views/bg9k-emty/files/4a66c672-a92a-4ced-82a2-033c28581a90?content_type=application%2Fzip%3B%20charset%3Dbinary&filename=Hospital_Revised_Flatfiles.zip" -O medicare.zip
-
