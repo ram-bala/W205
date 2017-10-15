7a7fbd32306227c60d1368d117b7ffb29dc330bc
Ram Balasubramanian <rbalasub@gmail.com>
2017-10-15 13:15:59 +0000

    loaded ddl 4 tables

---
+++ exercise_1/loading_and_modeling/show_tables.sql
@@ -0,0 +1,7 @@
+show tables;
+describe hospitals_gen_info;
+select * from hospitals_gen_info limit 10;
+describe hcahps_hospital;
+select * from hcaphs_hospital limit 10;
+describe comp_deaths_hospital;
+select * from comp_deaths_hospital limit 10;
