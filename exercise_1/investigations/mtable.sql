d99ea92392d258396c59b4df1fbc26517aca8e08
Ram Balasubramanian <rbalasub@gmail.com>
2017-10-27 03:43:45 +0000

    transformation and investigations

---
+++ exercise_1/investigations/mtable.sql
@@ -0,0 +1,2 @@
+select distinct measure_id, measure_name, measure_category 
+from tt_hospital_qos_measure;
---
+++ exercise_1/transforming/measurestobecoded.sql
@@ -0,0 +1,1 @@
+select distinct measure_id, measure_category from tt_hospital_qos_measure;
